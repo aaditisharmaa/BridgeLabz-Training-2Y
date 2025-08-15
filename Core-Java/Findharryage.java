4b093b25de61386a4286ab91809d6375b3863d19
aaditisharmaa <aditisharma19007@gmail.com>
2025-08-15 15:21:56 +0530

    1st

---
+++ Core-Java/Findharryage.java
@@ -0,0 +1,9 @@
+class Findharryage {
+    public static void main(String[] args){
+        int by = 2000;
+        int py = 2024;
+        int age = py - by;
+
+        System.out.println(" Harry's age in 2024 is " + age);
+    }
+}
