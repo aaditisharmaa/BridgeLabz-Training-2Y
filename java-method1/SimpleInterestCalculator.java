edb3291b7300b481e8ba0aa97c40176e940082d5
aaditisharmaa <aditisharma19007@gmail.com>
2025-09-04 11:49:49 +0530

    [aditi]-Add.SimpleInterestCalculator added

--- java-method1/SimpleInterestCalculator.java
+++ java-method1/SimpleInterestCalculator.java
@@ -1,29 +1,19 @@
 import java.util.Scanner;
 
 class SimpleInterestCalculator {
-    public double calculateSimpleInterest(double principal, double rate, double time) {
-        return (principal * rate * time) / 100;
+    public static double calculateSimpleInterest( double p , double r , double t){
+        return (p*r*t)/100;
     }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter principle , rate, time one bye one");
+        double p = sc.nextDouble();
+        double r = sc.nextDouble();        
+        double t = sc.nextDouble();
+        double SimpleInterest = calculateSimpleInterest(p ,r, t);
 
-
-        System.out.print("Enter Principal: ");
-        double principal = scanner.nextDouble();
-
-        System.out.print("Enter Rate of Interest: ");
-        double rate = scanner.nextDouble();
-
-        System.out.print("Enter Time (in years): ");
-        double time = scanner.nextDouble();
-
-        SimpleInterestCalculator calculator = new SimpleInterestCalculator();
-        double interest = calculator.calculateSimpleInterest(principal, rate, time);
-
-        System.out.println("The Simple Interest is " + interest +
-                           " for Principal " + principal +
-                           ", Rate of Interest " + rate +
-                           " and Time " + time );
+        System.out.println("The Simple Interest is "+ SimpleInterest + "for Principal " + p + ",Rate of Interest"+ r + "and Time " + t);
+        sc.close();
     }
+
 }
