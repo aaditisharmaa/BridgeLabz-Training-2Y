d27bec3a846072aca18307e2df134275d8205b8e
aaditisharmaa <aditisharma19007@gmail.com>
2025-09-07 23:05:04 +0530

    [aditi]-Add.MaxOfThree added

---
+++ Built-In-Function1/MaxOfThree.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class MaxOfThree {
+    public static int getMax(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        System.out.println("Maximum is: " + getMax(a, b, c));
+    }
+}
