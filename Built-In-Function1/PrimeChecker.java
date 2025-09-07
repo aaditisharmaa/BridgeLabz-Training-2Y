611f013c4c4197b766d46bc3b8246ee65826b3a8
aaditisharmaa <aditisharma19007@gmail.com>
2025-09-07 23:06:23 +0530

    [aditi]-Add.PrimeChecker added

---
+++ Built-In-Function1/PrimeChecker.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class PrimeChecker {
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println(n + " is prime? " + isPrime(n));
+    }
+}
