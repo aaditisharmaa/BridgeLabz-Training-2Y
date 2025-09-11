355b327a9e34a62dadcdc60f442a1454c4da534b
aaditisharmaa <aditisharma19007@gmail.com>
2025-09-11 23:12:27 +0530

    [aditi]-Add.MovieTicket added

---
+++ JavaClassandObject1/MovieTicket.java
@@ -0,0 +1,31 @@
+public class MovieTicket {
+    private String movieName;
+    private int seatNumber;
+    private double price;
+
+    public MovieTicket(String movieName, int seatNumber, double price) {
+        this.movieName = movieName;
+        this.seatNumber = seatNumber;
+        this.price = price;
+    }
+
+    public void bookTicket(String movieName, int seatNumber, double price) {
+        this.movieName = movieName;
+        this.seatNumber = seatNumber;
+        this.price = price;
+        System.out.println("Ticket Booked Successfully!");
+    }
+
+    public void displayDetails() {
+        System.out.println("Movie: " + movieName);
+        System.out.println("Seat Number: " + seatNumber);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        MovieTicket ticket = new MovieTicket("Avengers", 12, 250);
+        ticket.displayDetails();
+        ticket.bookTicket("Batman", 20, 300);
+        ticket.displayDetails();
+    }
+}
