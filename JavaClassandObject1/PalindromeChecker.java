3ebb8c9d32a7c5c271187135a2e34d8372fb0607
aaditisharmaa <aditisharma19007@gmail.com>
2025-09-11 23:09:21 +0530

    [aditi]-Add.PalindromeCheckeradded

---
+++ JavaClassandObject1/PalindromeChecker.java
@@ -0,0 +1,26 @@
+public class PalindromeChecker {
+    private String text;
+
+    public PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+    public boolean isPalindrome() {
+        String reversed = new StringBuilder(text).reverse().toString();
+        return text.equalsIgnoreCase(reversed);
+    }
+
+    public void displayResult() {
+        if (isPalindrome())
+            System.out.println(text + " is a palindrome.");
+        else
+            System.out.println(text + " is not a palindrome.");
+    }
+
+    public static void main(String[] args) {
+        PalindromeChecker pc1 = new PalindromeChecker("madam");
+        pc1.displayResult();
+        PalindromeChecker pc2 = new PalindromeChecker("hello");
+        pc2.displayResult();
+    }
+}
