caed718f4300f45617f95d3673c077b18c230f21
aaditisharmaa <aditisharma19007@gmail.com>
2025-09-13 23:22:47 +0530

    [aditi]-add.Employee added

---
+++ class-object/Employee.java
@@ -0,0 +1,25 @@
+public class Employee {
+    private String name;
+    private int id;
+    private double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee("John", 101, 50000);
+        Employee e2 = new Employee("Alice", 102, 60000);
+
+        e1.displayDetails();
+        e2.displayDetails();
+    }
+}
