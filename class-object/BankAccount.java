3ffb7368a0c1c35cb1c4bb3032c074b9f1e88d92
aaditisharmaa <aditisharma19007@gmail.com>
2025-09-13 23:48:58 +0530

    [aditi]-add.BankAccount added

---
+++ class-object/BankAccount.java
@@ -0,0 +1,36 @@
+public class BankAccount {
+    private String accountHolder;
+    private int accountNumber;
+    private double balance;
+
+    public BankAccount(String accountHolder, int accountNumber, double balance) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient Balance");
+        }
+    }
+
+    public void displayBalance() {
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: " + balance);
+    }
+
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount("Aditi", 12345, 1000.0);
+        account.deposit(500);
+        account.withdraw(300);
+        account.displayBalance();
+    }
+}
